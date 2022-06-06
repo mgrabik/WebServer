1da28fa6df7b127dd19544d0779676b0e135cecf
Mateusz Grabik <m.grabik@stud.elka.pw.edu.pl>
2022-06-06 14:51:00 +0200

    Newest Version

--- Data/Database.cs
+++ Data/Database.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using WebServer.Data.Dto;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Text;
 
 namespace WebServer.Data
 {
@@ -12,11 +18,18 @@ namespace WebServer.Data
         public List<SoilTemperatureDto> soilTemperatureDataSet = new List<SoilTemperatureDto>();
         public List<SoilHumidityDto> soilHumidityDataSet = new List<SoilHumidityDto>();
 
-        private const string connectionString = @"Data Source=DESKTOP-5M6N983\SQLEXPRESS;Initial Catalog=test_database;Integrated Security=True";
+        public double LiveAirTemperature;
+        public double LiveAirHumidity;
+        public double LiveSoilTemperature;
+        public double LiveSoilHumidity;
 
         private Database()
         {
-            getAllData();
+            setFirebaseClient();
+            SelectFireBaseData();
+            GetLiveData();
+            //Thread thread = new Thread(SecondFoo);
+            //thread.Start();
         }
 
         // Singleton implementation
@@ -33,33 +46,122 @@ namespace WebServer.Data
             }
         }
         
-
-        public void getAllData()
+        IFirebaseConfig fcon = new FirebaseConfig()
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            AuthSecret = "S9amkALnOl1aZTy4PAM5I47tjcE1FTHMhHDSf6RH",
+            BasePath = "https://incz-soilmaster-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient fclinet;
+
+        private void setFirebaseClient()
+        {
+            try
             {
-                connection.Open();
-                SqlCommand command = new SqlCommand("SELECT * FROM [Measurements] ORDER BY [Timestamp] DESC", connection);
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        DateTime timestamp = (DateTime)reader["Timestamp"];
-
-                        AirTemperatureDto airTemperatureDto = new AirTemperatureDto(Double.Parse(String.Format("{0}", reader["airTemperature"])), timestamp);
-                        airTemperatureDataSet.Add(airTemperatureDto);
-
-                        AirHumidityDto airHumidityDto = new AirHumidityDto(Double.Parse(String.Format("{0}", reader["airHumidity"])), timestamp);
-                        airHumidityDataSet.Add(airHumidityDto);
-
-                        SoilTemperatureDto soilTemperatureDto = new SoilTemperatureDto(Double.Parse(String.Format("{0}", reader["soilTemperature"])), timestamp);
-                        soilTemperatureDataSet.Add(soilTemperatureDto);
-
-                        SoilHumidityDto soilHumidityDto = new SoilHumidityDto(Double.Parse(String.Format("{0}", reader["soilHumidity"])), timestamp);
-                        soilHumidityDataSet.Add(soilHumidityDto);
-                    }
-                }
+                fclinet = new FireSharp.FirebaseClient(fcon);
             }
+            catch
+            {
+                throw new Exception("There was a problem in the internet!");
+            }
+        }
+        public void SelectFireBaseData()
+        {
+            clearDatabase();
+            var result = fclinet.Get("Data");
+            var json = result.ResultAs < Dictionary<string, Dictionary<string, string>>>();
+            var allDictionaries = json.Values;
+            foreach (var element in allDictionaries)
+            {
+                //Validation(element["timestamp"])
+                DateTime timestamp = DateTime.ParseExact(Validation(element["timestamp"]), "yyyy-MM-dd HH:mm:ss ", null);
+                
+                airTemperatureDataSet.Add(new AirTemperatureDto(Convert.ToDouble(Regex.Replace(element["temperatura"], @"\.", ",")), timestamp));
+                airHumidityDataSet.Add(new AirHumidityDto(Convert.ToDouble(Regex.Replace(element["wilgotnosc"], @"\.", ",")), timestamp));
+                soilTemperatureDataSet.Add(new SoilTemperatureDto(Convert.ToDouble(Regex.Replace(element["temperatura_gleby"], @"\.", ",")), timestamp));
+                soilHumidityDataSet.Add(new SoilHumidityDto(Convert.ToDouble(Regex.Replace(element["wilgotnosc_gleby"], @"\.", ",")), timestamp));
+            }
+        }
+        public void GetLiveData()
+        {
+            var result = fclinet.Get("Data");
+            var json = result.ResultAs<Dictionary<string, Dictionary<string, string>>>();
+            var allDictionaries = json.Values;
+            int maxiteration = allDictionaries.Count;
+            int i = 0;
+            foreach (var element in allDictionaries)
+            {
+                if (i < maxiteration - 1)
+                {
+                    i++;
+                    continue;
+                }
+                LiveAirTemperature = Convert.ToDouble(Regex.Replace(element["temperatura"], @"\.", ","));
+                LiveAirHumidity = Convert.ToDouble(Regex.Replace(element["wilgotnosc"], @"\.", ","));
+                LiveSoilTemperature = Convert.ToDouble(Regex.Replace(element["temperatura_gleby"], @"\.", ","));
+                LiveSoilHumidity = Convert.ToDouble(Regex.Replace(element["wilgotnosc_gleby"], @"\.", ","));
+                break;
+            }
+        }
+
+        private string Validation(string date)
+        {
+            var month = Regex.Match(date, "-(.*?)-(.*?) ").Groups[1];
+            var day = Regex.Match(date, "-(.*?)-(.*?) ").Groups[2];
+            var hour = Regex.Match(date, " (.*?):").Groups[1];
+            var minute = Regex.Match(date, ":(.*?):").Groups[1];
+            var second = Regex.Match(date, ":(.*?):(.*?) ").Groups[2];
+
+            var regex = new Regex(Regex.Escape("-"));
+            string result = regex.Replace(date, "%", 1);
+
+            regex = new Regex(Regex.Escape(":"));
+            result = regex.Replace(result, "#", 1);
+
+            if (month.Length == 1)
+            {
+                string newMonth = "%0" + month.Value + "-";
+                result = Regex.Replace(result, "%(.*?)-", newMonth);
+            }
+            if (day.Length == 1)
+            {
+                string newDay = "-0" + day.Value + " ";
+                result = Regex.Replace(result, "-(.*?) ", newDay);
+            }
+            if (hour.Length == 1)
+            {
+                string newHour = " 0" + day.Value + ":";
+                result = Regex.Replace(result, "(.*?)#", newHour);
+            }
+            if (minute.Length == 1)
+            {
+                string newMinute = ":0" + day.Value + ":";
+                result = Regex.Replace(result, "#(.*?):", newMinute);
+            }
+            if (second.Length == 1)
+            {
+                string newSecond = ":0" + day.Value + " ";
+                result = Regex.Replace(result, ":(.*?) ", newSecond);
+            }
+            regex = new Regex(Regex.Escape("%"));
+            string result3 = regex.Replace(result, "-");
+            regex = new Regex(Regex.Escape("#"));
+            string result4 = regex.Replace(result3, ":");
+
+            return result4;
+        }
+
+        private void clearDatabase()
+        {
+            airTemperatureDataSet.Clear();
+            airHumidityDataSet.Clear();
+            soilTemperatureDataSet.Clear();
+            soilHumidityDataSet.Clear();
+        }
+
+        public int mylabel = 1;
+        public void SecondFoo()
+        {
+            mylabel++;
         }
     }
 }
---
+++ Data/Dto/usuntemp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebServer.Data.Dto
+{
+    public class usuntemp
+    {
+        public DateTime Date { get; set; }
+        public double asd { get; set; }
+        public usuntemp(double asd, DateTime date)
+        {
+            this.asd = asd;
+            this.Date = date;
+        }
+    }
+}
